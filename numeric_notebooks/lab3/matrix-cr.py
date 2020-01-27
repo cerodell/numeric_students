d142c87f288e6f73e25a89cf79a1b01a49df891c
cerodell <rodell@colorado.edu>
2020-01-27 10:36:50 -0800

    playing with matrix

--- numeric_notebooks/lab3/matrix-cr.py
+++ numeric_notebooks/lab3/matrix-cr.py
@@ -7,3 +7,30 @@ import numpy as np
 a = np.array([[0.0001, 0.0001, 0.5],[0.5, 1, 1], [0.0001, 1, 0.0001]])
 
 k = np.linalg.cond(a)
+
+# b = np.array([1, 0.0001, 0.5],[0.5, 1, 1], [0.0001, 1, 0.0001]])
+
+
+def diffmatrix(n):
+    x = np.zeros(n)
+    for i in x:
+        B = np.array([(i+1),(-2*i),(i-1)])
+    return B
+
+B =diffmatrix(50)
+
+
+
+# x = range(6)
+
+# for n in x:
+#   print(n)
+
+
+    # n = A.shape[0]
+    # E = np.eye(n)
+    # if i == j:
+    #     E[i,i] = k + 1
+    # else:
+    #     E[i,j] = k
+    # return E @ A

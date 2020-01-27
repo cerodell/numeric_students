2335c1aa995a20963907d546f125ea6553a529de
cerodell <rodell@colorado.edu>
2020-01-27 13:20:42 -0800

    matrix stuggles

--- numeric_notebooks/lab3/matrix-cr.py
+++ numeric_notebooks/lab3/matrix-cr.py
@@ -4,20 +4,20 @@ import numpy as np
 
 
 
-a = np.array([[0.0001, 0.0001, 0.5],[0.5, 1, 1], [0.0001, 1, 0.0001]])
+# a = np.array([[0.0001, 0.0001, 0.5],[0.5, 1, 1], [0.0001, 1, 0.0001]])
 
-k = np.linalg.cond(a)
+# k = np.linalg.cond(a)
 
 # b = np.array([1, 0.0001, 0.5],[0.5, 1, 1], [0.0001, 1, 0.0001]])
 
 
-def diffmatrix(n):
-    x = np.zeros(n)
+def diffmatrix(n,m):
+    x = np.zeros((n,m))
     for i in x:
         B = np.array([(i+1),(-2*i),(i-1)])
     return B
 
-B =diffmatrix(50)
+B =diffmatrix(50,40)
 
 
 

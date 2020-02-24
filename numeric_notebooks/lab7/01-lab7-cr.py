15e34247fd5a2bbe008ada037284d796fba8c9ad
cerodell <rodell@colorado.edu>
2020-02-24 13:42:55 -0800

    start of lab 7

---
+++ numeric_notebooks/lab7/01-lab7-cr.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     formats: ipynb,py:percent
+#     notebook_metadata_filter: all,-language_info,-toc,-latex_envs
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Laboratory 7: Solving partial differential equations using an explicit, finite difference method.
+# ## Christopher Rodell
+# ### EOSC 511
+
+# %% [markdown]
+# ## Problem Two
+# - a\) Find the CFL condition (in seconds) for 𝑑𝑡 for the Python example in Problem One. Test your value.

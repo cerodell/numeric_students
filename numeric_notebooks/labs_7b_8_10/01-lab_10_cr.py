9a12ca518c779702fcfdc19de1e6213e3d80be9a
Chris Rodell <crodell@eoas.ubc.ca>
2020-03-17 20:36:03 -0700

    lab 10 started

---
+++ numeric_notebooks/labs_7b_8_10/01-lab_10_cr.py
@@ -0,0 +1,18 @@
+# %% [markdown]
+# # Lab 10
+### EOSC 511
+#### Christopher Rodell
+# %% [markdown]
+# ## Problem One
+# Using the scheme presented in this section, modify advection3.py 
+# to solve the following advection problem: The wind is moving along
+# the x-axis with speed 𝑢=20 m/s. The initial distribution curve is 
+# 290 km in width. Use your program to approximate the curve during 24 hours.
+#
+# $$
+# \\
+# $$
+# - a\) Run your program for different orders of approximating polynomials (up to 4).
+#  Compare the accuracy of approximation for different orders. Do you see better 
+#  results with increasing order? Is this true for all orders from 0 to 4?
+#   Is there any particularity to odd and even order polynomials?
